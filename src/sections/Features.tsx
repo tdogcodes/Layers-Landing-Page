effcdbf1db82f8d2b0c7fae208d5b385ff51998f
tdogcodes <trxcycsgo@gmail.com>
2025-02-03 15:45:36 -0800

    responsive features section

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -22,9 +22,9 @@ export default function Features() {
         <section className="py-24 ">
             <div className="container">
                 <div className="flex justify-center"><Tag>Features</Tag></div>
-                <h2 className="text-6xl font-medium text-center mt-6">Where power meets <span className="text-lime-400">simplicity</span></h2>
-                <div className="mt-12 grid grid-cols-1 md:grid-cols-4 gap-8">
-                    <FeatureCard title="Real-time Collaboration" description="Work together seamlessly with conflict-free team editing" className="col-span-2">
+                <h2 className="text-6xl font-medium text-center mt-6 mx-auto max-w-2xl">Where power meets <span className="text-lime-400">simplicity</span></h2>
+                <div className="mt-12 grid grid-cols-1 md:grid-cols-4 lg:grid-cols-3 gap-8">
+                    <FeatureCard title="Real-time Collaboration" description="Work together seamlessly with conflict-free team editing" className="md:col-span-2 lg:col-span-1">
                         <div className="aspect-video flex items-center justify-center ">
                             <Avatar className="z-40">
                                 <Image src={Avatar1} alt="avatar 1" className="rounded-full"/>
@@ -44,13 +44,13 @@ export default function Features() {
                             </Avatar>
                         </div>
                     </FeatureCard>
-                    <FeatureCard title="Interactive Prototyping" description="Engage your clients with prototypes that react to user actions" className="col-span-2">
+                    <FeatureCard title="Interactive Prototyping" description="Engage your clients with prototypes that react to user actions" className="md:col-span-2 lg:col-span-1">
                         <div className="aspect-video flex items-center">
                             <p className="text-4xl font-extrabold text-white/20 text-center">We've achieved{" "}
                             <span className="bg-gradient-to-r from-purple-400 to-pink-400 bg-clip-text text-transparent">incredible</span> growth this year</p>
                         </div>
                     </FeatureCard>
-                    <FeatureCard title="Keyboard Quick Actions" description="Powerful commands to help you create designs more quickly" className="col-span-2 col-start-2">
+                    <FeatureCard title="Keyboard Quick Actions" description="Powerful commands to help you create designs more quickly" className="md:col-span-2 md:col-start-2 lg:col-span-1 lg:col-start-auto">
                         <div className="aspect-video flex items-center justify-center gap-4">
                             <Key className="w-28">Shift</Key>
                             <Key>Alt</Key>
@@ -60,9 +60,9 @@ export default function Features() {
                 </div>
                 <div className="mt-8 flex flex-wrap gap-3 justify-center">
                     {features.map((feature) => (
-                        <div key={feature} className="bg-neutral-900 border border-white/10 inline-flex px-3 py-1.5 rounded-2xl gap-3 items-center">
+                        <div key={feature} className="bg-neutral-900 border border-white/10 inline-flex px-3 md:px-5 py-1.5 md:py-2 rounded-2xl gap-3 items-center">
                             <span className="bg-lime-400 text-xl text-neutral-950 size-5 rounded-full inline-flex items-center justify-center">&#10038;</span>
-                            <span className="font-medium">{feature}</span>
+                            <span className="font-medium md:text-lg">{feature}</span>
                         </div>
                     ))}
                 </div>
