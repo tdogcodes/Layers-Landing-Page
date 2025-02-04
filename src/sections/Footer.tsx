8fb37d4b2a49fffe63efbe768d78d10f24a92ab3
tdogcodes <trxcycsgo@gmail.com>
2025-02-03 23:44:12 -0800

    design complete

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,6 @@
+import Image from 'next/image';
+import logoImage from '@/assets/images/logo.svg'
+
 const footerLinks = [
     { href: "#", label: "Contact" },
     { href: "#", label: "Privacy Policy" },
@@ -5,5 +8,20 @@ const footerLinks = [
 ];
 
 export default function Footer() {
-    return <div>Footer</div>;
+    return <section className='py-16'>
+        <div className="container">
+            <div className='flex flex-col items-center gap-6 md:flex-row md:justify-between'>
+                <div>
+                    <Image src={logoImage} alt="logo"/>
+                </div>
+                <div>
+                    <nav className='flex gap-6'>
+                        {footerLinks.map((link, i) =>(
+                            <a key={i} href={link.href} className='text-white/50 text-sm'>{link.label}</a>
+                        ))}
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </section>;
 }
