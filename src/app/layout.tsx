27d6ee73a9f1cff16db27ae4e33d274368091c4e
tdogcodes <trxcycsgo@gmail.com>
2025-04-06 18:24:00 -0700

    finished

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,8 +10,7 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-    title: "Modern Design Tool Landing Page",
-    description: "Created with the help of Frontend Tribe",
+    title: "Modern Design Tool Landing Page"
 };
 
 export default function RootLayout({
